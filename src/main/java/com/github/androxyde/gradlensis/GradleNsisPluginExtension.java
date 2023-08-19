5ff9cfc0d0dc4613d5ba30ed827fcbf4af8255d0
Androxyde <s1flashtool@gmail.com>
2023-08-19 20:00:19 +0200

    init

---
+++ src/main/java/com/github/androxyde/gradlensis/GradleNsisPluginExtension.java
@@ -0,0 +1,89 @@
+package com.github.androxyde.gradlensis;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.gradle.api.Project;
+import org.gradle.api.Transformer;
+import org.gradle.api.file.Directory;
+import org.gradle.api.file.DirectoryProperty;
+import org.gradle.api.file.RegularFile;
+import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.provider.MapProperty;
+import org.gradle.api.provider.Property;
+import org.gradle.api.provider.SetProperty;
+import org.gradle.api.tasks.Optional;
+
+public class GradleNsisPluginExtension 
+{
+	private static final String BINARY_DIRECTORY = "tmp/nsis";
+	private final RegularFileProperty configuration;
+	private final DirectoryProperty extractTo;
+	private final DirectoryProperty runIn;
+	private final DirectoryProperty destinationFolder;
+	private final MapProperty<String, String> variables;
+	private final SetProperty<String> additionalPlugins;
+	private final Property<String> destinationName;
+    public GradleNsisPluginExtension(final Project project) {
+    	configuration = project.getObjects().fileProperty();
+    	configuration.convention(project.getLayout().getProjectDirectory().file("config.nsis"));
+    	
+    	runIn = project.getObjects().directoryProperty();
+    	runIn.convention(configuration.map(new Transformer<Directory, RegularFile>() 
+    	{
+			@Override
+			public Directory transform(RegularFile org) 
+			{
+				String relative = project.getProjectDir().toURI().relativize(org.getAsFile().getParentFile().toURI()).getPath();
+				if(relative != null && relative.length()>0)
+					return project.getLayout().getProjectDirectory().dir(relative);
+				else
+					return project.getLayout().getProjectDirectory();
+			}
+		}));
+    	
+    	extractTo = project.getObjects().directoryProperty();
+    	extractTo.convention(project.getLayout().getBuildDirectory().dir(BINARY_DIRECTORY));
+    	
+    	destinationName = project.getObjects().property(String.class);
+    	
+    	destinationFolder = project.getObjects().directoryProperty();
+    	destinationFolder.convention(project.getLayout().getBuildDirectory().dir("distributions"));
+    	
+    	variables = project.getObjects().mapProperty(String.class, String.class);
+    	variables.convention(new HashMap<String, String>());
+    	
+    	additionalPlugins = project.getObjects().setProperty(String.class);
+    	additionalPlugins.convention(new ArrayList<String>());
+    }
+    @Optional
+    public DirectoryProperty getExtractTo() {
+        return extractTo;
+    }
+    @Optional
+    public Property<String> getDestinationName()
+    {
+    	return destinationName;
+    }
+    @Optional
+    public DirectoryProperty getDestinationFolder() {
+        return destinationFolder;
+    }
+    @Optional
+    public DirectoryProperty getRunIn() {
+        return runIn;
+    }
+    @Optional
+    public MapProperty<String, String> getVariables() {
+        return variables;
+    }
+    @Optional
+    public SetProperty<String> getAdditionalPlugins() {
+        return additionalPlugins;
+    }
+    @Optional
+    public RegularFileProperty getConfiguration() {
+        return configuration;
+    }
+
+}
---
+++ src/main/java/com/github/androxyde/gradlensis/NsisExecutor.java
@@ -0,0 +1,192 @@
+package com.github.androxyde.gradlensis;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.gradle.api.Project;
+import org.gradle.api.Action;
+import org.gradle.api.GradleScriptException;
+import org.gradle.api.file.CopySpec;
+import org.gradle.api.file.Directory;
+import org.gradle.api.file.FileCopyDetails;
+import org.gradle.api.file.FileTree;
+import org.gradle.api.file.RegularFile;
+import org.gradle.api.file.RelativePath;
+import org.gradle.api.logging.LogLevel;
+import org.gradle.process.ExecResult;
+import org.gradle.process.ExecSpec;
+
+class NsisExecutor 
+{
+	private static final String SOURCE_NSIS_FOLDER = "nsis-3.0.9";
+	private static final String SOURCE_PLUGINS_FOLDER = "plugins";
+	private static final String DEST_PLUGINS_FOLDER = "Plugins";
+	private static final String DEST_PLUGINS_ANSI_FOLDER = "x86-ansi";
+	@SuppressWarnings("unused")
+	private static final String DEST_PLUGINS_UNICODE_FOLDER = "x86-unicode";
+	static RegularFile execute(final Project project, final Directory extractTo, final Directory runIn, final Map<String, String> variables, final Set<String> additionalPlugins, final RegularFile configuration) throws GradleScriptException
+	{
+		final RegularFile makensisPath = extract(project, extractTo);
+		for(String additionalPlugin : additionalPlugins)
+		{
+			extractPlugin(project, additionalPlugin, extractTo);
+		}
+		final ByteArrayOutputStream logger = new ByteArrayOutputStream()
+		{
+			@Override
+			public void write(int b)
+			{
+				System.out.write(b);
+				super.write(b);
+			}
+			@Override
+			public void write(byte[] b, int off, int len)
+			{
+				System.out.write(b, off, len);
+				super.write(b, off, len);
+			}
+			@Override
+			public void write(byte[] b) throws IOException
+			{
+				System.out.write(b);
+				super.write(b);
+			}
+		};
+		ExecResult result = project.exec(new Action<ExecSpec>() 
+		{
+			@Override
+			public void execute(ExecSpec spec) {
+				spec.setExecutable(makensisPath);
+				spec.setWorkingDir(runIn);
+				// prevent NSIS from automatically changing current path to NSIS configuration file
+				spec.args("/NOCD");
+				for(Map.Entry<String, String> entry : variables.entrySet())
+				{
+					spec.args("/D"+entry.getKey()+"="+entry.getValue());
+				}
+				spec.args(configuration.toString());
+				spec.setStandardOutput(logger);
+			}
+		});
+		result.assertNormalExitValue();
+		// Find part in NSIS console output where it is stated how the generated installer exe file is named
+		Pattern installerFilePattern = Pattern.compile("Output: \"([^\"$]{4,})\"");
+		Matcher matcher = installerFilePattern.matcher(logger.toString());
+		if(!matcher.find())
+			throw new GradleScriptException("Couldn't find out where installer was created by NSIS.", null);
+		String installerPath = matcher.group(1);
+		String relative = project.getProjectDir().toURI().relativize(new File(installerPath).toURI()).getPath();
+		return project.getLayout().getProjectDirectory().file(relative);
+	}
+	private static RegularFile extract(final Project project, final Directory extractTo) throws GradleScriptException
+	{
+		RegularFile makensisPath = extractTo.file("makensis.exe");
+		if(makensisPath.getAsFile().exists())
+		{
+			project.getLogger().log(LogLevel.INFO, "NSIS already extracted to: " + extractTo);
+			return makensisPath;
+		}
+		project.getLogger().log(LogLevel.INFO, "Extracting NSIS to: " + extractTo);
+		File pathToJar;
+		try 
+		{
+			pathToJar = new File(NsisExecutor.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+		} 
+		catch (URISyntaxException e) {
+			throw new GradleScriptException("Could not find out path to JAR of plugin.", e);
+		}
+		final FileTree jarContent = project.zipTree(pathToJar);
+		project.copy(new Action<CopySpec>(){
+
+			@Override
+			public void execute(CopySpec spec) 
+			{
+				spec.from(jarContent);
+				spec.setIncludeEmptyDirs(false);
+				spec.into(extractTo);
+				spec.eachFile(new Action<FileCopyDetails>() {
+
+					@Override
+					public void execute(FileCopyDetails fcd) {
+						if(fcd.getRelativePath().getPathString().startsWith(SOURCE_NSIS_FOLDER))
+						{
+							String[] pathElems = fcd.getRelativePath().getSegments();
+							// Delete first element: That's "bin", we already know that
+							pathElems = Arrays.copyOfRange(pathElems, 1, pathElems.length);
+							fcd.setRelativePath(new RelativePath(!fcd.getFile().isDirectory(), pathElems));
+			                fcd.setMode(0755);
+						}
+						else
+							fcd.exclude();
+					}
+					
+				});
+			}
+		});
+		if(!makensisPath.getAsFile().exists())
+			throw new GradleScriptException("Extracted NSIS files to "+extractTo.toString()+", but file "+makensisPath.toString()+" cannot be found.", null);
+		return makensisPath;
+	}
+	private static void extractPlugin(final Project project, final String pluginName, final Directory extractTo) throws GradleScriptException
+	{
+		RegularFile pluginDllPath = extractTo.file(DEST_PLUGINS_FOLDER + "/" + DEST_PLUGINS_ANSI_FOLDER+"/"+pluginName+".dll");
+		if(pluginDllPath.getAsFile().exists())
+		{
+			project.getLogger().log(LogLevel.INFO, "Plugin "+pluginName+" already installed");
+			return;
+		}
+		project.getLogger().log(LogLevel.INFO, "Extracting Plugin " + pluginName);
+		File pathToJar;
+		try 
+		{
+			pathToJar = new File(NsisExecutor.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+		} 
+		catch (URISyntaxException e) {
+			throw new GradleScriptException("Could not find out path to JAR of plugin.", e);
+		}
+		final Directory dest = extractTo.dir(DEST_PLUGINS_FOLDER);
+		final FileTree jarContent = project.zipTree(pathToJar);
+		class FoundPluginIndicator
+		{
+			boolean found = false;
+		}
+		final FoundPluginIndicator foundPlugin = new FoundPluginIndicator();
+		project.copy(new Action<CopySpec>(){
+
+			@Override
+			public void execute(CopySpec spec) 
+			{
+				spec.from(jarContent);
+				spec.setIncludeEmptyDirs(false);
+				spec.into(dest);
+				spec.eachFile(new Action<FileCopyDetails>() {
+
+					@Override
+					public void execute(FileCopyDetails fcd) {
+						if(fcd.getRelativePath().getPathString().startsWith(SOURCE_PLUGINS_FOLDER+"/"+pluginName))
+						{
+							String[] pathElems = fcd.getRelativePath().getSegments();
+							// Delete first element: That's "bin", we already know that
+							pathElems = Arrays.copyOfRange(pathElems, 2, pathElems.length);
+							fcd.setRelativePath(new RelativePath(!fcd.getFile().isDirectory(), pathElems));
+			                fcd.setMode(0755);
+			                foundPlugin.found = true;
+						}
+						else
+							fcd.exclude();
+					}
+					
+				});
+			}
+		});
+		if(!foundPlugin.found)
+			throw new GradleScriptException("Could not find plugin "+pluginName + ". Please check spelling.", null);
+	}
+}
